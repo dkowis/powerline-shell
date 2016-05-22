1277725367408e8aa8398ba7c7db8ad3f95f22f4
David Kowis <dkowis@shlrm.org>
2016-05-22 16:45:50 -0500

    Adding the word "ruby" to it

--- segments/rbenv.py
+++ segments/rbenv.py
@@ -3,7 +3,7 @@ import subprocess
 def add_rbenv_segment(powerline):
     try:
         p1 = subprocess.Popen(["rbenv", "version-name"], stdout=subprocess.PIPE)
-        version = p1.communicate()[0].rstrip()
+        version = "ruby " + p1.communicate()[0].rstrip()
         powerline.append(version, 15, 1)
     except OSError:
         return
